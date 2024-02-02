ad7ecbf4733bfbdca8edf6470d2e3d9c2e2c58e8
Yash Ghugardare <yghugardare6@gmail.com>
2024-02-02 16:22:43 +0530

    user model done

---
+++ server/models/user.model.ts
@@ -0,0 +1,95 @@
+import mongoose, { Document, Model } from "mongoose";
+import bcrypt from "bcryptjs";
+require("dotenv").config();
+
+const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+// IUser - it represents the structure of a user document
+// it extends the mogoose Document class, indicating that
+// IUser inherits  all methods and properties like ,
+// "save", "update" , "delete",etc of the mongoose document
+export interface IUser extends Document {
+  // we will define the user document properites and its
+  // types
+  name: string;
+  email: string;
+  password: string;
+  avatar: {
+    public_id: string;
+    url: string;
+  };
+  role: string;
+  isVerified: boolean;
+  courses: Array<{ courseId: string }>;
+  comparePassword: (password: string) => Promise<boolean>;
+  SignAccessToken: () => string;
+  SignRefreshToken: () => string;
+}
+// Creating schema instance that follow IUser
+// user schema is instance of Schema that follows IUser interface
+const userSchema = new mongoose.Schema<IUser>(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      validate: {
+        validator: function (value: string) {
+          return emailRegexPattern.test(value);
+        },
+        message: "please enter a valid email",
+      },
+      unique: true,
+    },
+    password: {
+      type: String,
+      minlength: [6, "Password must be at least 6 characters"],
+      // do not return this field to the user from query result
+      select: false,
+    },
+    avatar: {
+      // a unique identifier for the image in Cloudinary
+      public_id: String,
+      // URL of the image in Cloudinary
+      url: String,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    courses: [
+      {
+        courseId: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+// <IUser> help TS understrand that "this" refers
+// mongodb document which has properties defined in the
+// IUser interface
+userSchema.pre<IUser>("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  // if not modified hash it before saving to db
+  this.password = await bcrypt.hash(this.password, 10);
+  // then save
+  next();
+});
+// compare password
+userSchema.methods.comparePassword = async function (
+  enteredPassword: string
+): Promise<boolean> {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+const userModel: Model<IUser> = mongoose.model("User", userSchema);
