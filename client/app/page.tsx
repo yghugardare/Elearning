d9e7c9b618a7c3adffe8a40253d393128b3fcffb
Yash Ghugardare <yghugardare6@gmail.com>
2024-02-13 17:25:31 +0530

    Basic Client Setup
    Made necessary client setup in nextjs, installed dependancies,
    configured css, downloaded assets , designed header and hero page of the wesbite

---
+++ client/app/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import React, { FC, useState } from "react";
+import { Heading } from "@/app/utils/Heading";
+import Header from "./components/Header";
+import Hero from "./components/Route/Hero";
+interface Props {}
+const Page: FC<Props> = () => {
+const [open, setOpen] = useState(false);
+const [activeItem, setActiveItem] = useState(0);
+
+  return (
+    <div>
+      <Heading
+        title="Elearning"
+        description="Elearning is a platform for students to learn and get help from teachers and Ai"
+        keywords="Programming,mern,nextjs,redux,AI"
+      />
+      <Header
+      open={open}
+      setOpen={setOpen}
+      activeItem={activeItem}
+      />
+      <Hero/>
+    </div>
+  );
+};
+export default Page;
