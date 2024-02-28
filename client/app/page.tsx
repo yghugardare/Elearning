3555e3f78e88518bf657c19632c3e7e6b8af2c91
Yash Ghugardare <yghugardare6@gmail.com>
2024-02-28 20:17:13 +0530

    ADD: Courses ,FAQ,Footer UI
    made the hero layoout dynamic
    
    add get course query,getFaq query
    
    added review ,faq and footer ui

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,10 +1,16 @@
 "use client";
-import React, { FC, useState } from "react";
-import { Heading } from "@/app/utils/Heading";
+import React, { FC, useEffect, useState } from "react";
+import {Heading} from "./utils/Heading";
 import Header from "./components/Header";
 import Hero from "./components/Route/Hero";
+import Courses from "./components/Route/Courses";
+import Reviews from "./components/Route/Reviews";
+import FAQ from "./components/FAQ/FAQ";
+import Footer from "./components/Footer";
+
 interface Props {}
-const Page: FC<Props> = () => {
+
+const Page: FC<Props> = (props) => {
   const [open, setOpen] = useState(false);
   const [activeItem, setActiveItem] = useState(0);
   const [route, setRoute] = useState("Login");
@@ -12,9 +18,9 @@ const Page: FC<Props> = () => {
   return (
     <div>
       <Heading
-        title="Elearning"
-        description="Elearning is a platform for students to learn and get help from teachers and Ai"
-        keywords="Programming,mern,nextjs,redux,AI"
+        title="ELearning"
+        description="ELearning is a platform for students to learn and get help from teachers"
+        keywords="Prograaming,MERN,Redux,Machine Learning"
       />
       <Header
         open={open}
@@ -24,7 +30,12 @@ const Page: FC<Props> = () => {
         route={route}
       />
       <Hero />
+      <Courses />
+      <Reviews />
+      <FAQ />
+      <Footer />
     </div>
   );
 };
+
 export default Page;
