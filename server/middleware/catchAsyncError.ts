6399ecdbffc74a0199175ee013ef9ada28712cf2
Yash Ghugardare <yghugardare6@gmail.com>
2026-08-06 22:26:48 +0530

    refactor: remove all the old code

--- server/middleware/catchAsyncError.ts
+++
@@ -1,6 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export const CatchAsyncError =
-  (theFunc: any) => (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(theFunc(req, res, next)).catch(next);
-  };
--- server/utils/ErrorHandler.ts
+++
@@ -1,9 +0,0 @@
-class ErrorHandler extends Error {
-  statusCode: Number;
-  constructor(message: any, statusCode: Number) {
-    super(message);
-    this.statusCode = statusCode;
-    Error.captureStackTrace(this, this.constructor);
-  }
-}
-export default ErrorHandler;
